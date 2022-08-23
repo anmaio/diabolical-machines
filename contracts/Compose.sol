5b648380693113f7e5172cf088012cc357241bf5
Danny <dannyp@anma.io>
2022-08-23 15:34:30 +0000

    fixed unit tests

--- contracts/Compose.sol
+++ contracts/Compose.sol
@@ -11,7 +11,8 @@ contract Compose {
 
     constructor() {
         _html.baseURI = "https://anma.mypinata.cloud/ipfs/QmcQ6J1rbMNWbaEGt5m5vWLB3VzYNCQiuJHFxc9iZJgmPt/";
-        _html.header = "<!DOCTYPE html> <html lang=''>  <meta charset='utf-8'>  <title>html-ipfs-test</title>  <head><style>img{position:absolute;top: 0px;left: 0px; height:100vh;}#layer1{z-index: 10;}#layer2{z-index: 20;}#layer3{z-index: 30;}#layer4{z-index: 40;}#layer5{z-index: 50;}#layer6{z-index: 60;}#layer7{z-index: 70;}</style></head> <body> <canvas id='c'></canvas>";
+        _html
+            .header = "<!DOCTYPE html> <html lang=''>  <meta charset='utf-8'>  <title>html-ipfs-test</title>  <head><style>img{position:absolute;top: 0px;left: 0px; height:100vh;}#layer1{z-index: 10;}#layer2{z-index: 20;}#layer3{z-index: 30;}#layer4{z-index: 40;}#layer5{z-index: 50;}#layer6{z-index: 60;}#layer7{z-index: 70;}</style></head> <body> <canvas id='c'></canvas>";
         _html.imgOpen = "<img id='layer";
         _html.imgSrc = "' src='";
         _html.imgClose = ".png'/>";
@@ -23,7 +24,16 @@ contract Compose {
         string memory output = _html.header;
         for (uint256 i = 0; i < 7; i++) {
             if (keccak256(abi.encodePacked(traitData[i])) != keccak256(abi.encodePacked("none"))) {
-                output = string.concat(output, _html.imgOpen, Strings.toString(i + 1), _html.imgSrc, _html.baseURI, folders[i], traitData[i], _html.imgClose);
+                output = string.concat(
+                    output,
+                    _html.imgOpen,
+                    Strings.toString(i + 1),
+                    _html.imgSrc,
+                    _html.baseURI,
+                    folders[i],
+                    traitData[i],
+                    _html.imgClose
+                );
             }
         }
         output = Base64.encode(bytes(string.concat(output, _html.footer)));
