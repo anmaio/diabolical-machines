202e1b10dd45e02e64764a938a11389d1eb1d1f6
Danny <dannyp@anma.io>
2022-09-26 14:13:34 +0000

    fixed test and remove unused code

--- contracts/Metadata.sol
+++ contracts/Metadata.sol
@@ -73,37 +73,37 @@ contract Metadata is Ownable {
     // probably only worth doing if we get a stack too deep error
 
     // alternative to generateRightWallPositions
-    function getRWPositions(uint256 _tokenId) public view returns (uint[] memory) {
-      uint uintVrf = _VRF.getNumberFromId(_tokenId);
-      bytes memory vrf = uintToBytes(uintVrf);
-      vrf = slice(vrf, 1, rightWallTraits.length * 2);
-      uint256 rand = uint256(keccak256(vrf));
-      // start with an empty grid
-      string[9] memory newGrid = noRow1Grid;
-      uint[] memory viewTraitPositons = new uint[](rightWallTraits.length);
-      for (uint256 i = 0; i < rightWallTraits.length; i++) {
-          uint256 probability = rand % 100;
-          uint256 index = 9;
-          if (probability < rightWallProbabilities[i]) {
-              // cannot go on the bottom row of the wall
-              uint256 position = (rand % 6) + 3;
-              (newGrid, index) = pickPosition(newGrid, rightWallTraits[i], position);
-          }
-          rand = rand / 100;
-          viewTraitPositons[i] = index;
-      }
-      return viewTraitPositons;
+    function getRWPositions(uint256 _tokenId) public view returns (uint256[] memory) {
+        uint256 uintVrf = _VRF.getNumberFromId(_tokenId);
+        bytes memory vrf = uintToBytes(uintVrf);
+        vrf = slice(vrf, 1, rightWallTraits.length * 2);
+        uint256 rand = uint256(keccak256(vrf));
+        // start with an empty grid
+        string[9] memory newGrid = noRow1Grid;
+        uint256[] memory viewTraitPositons = new uint256[](rightWallTraits.length);
+        for (uint256 i = 0; i < rightWallTraits.length; i++) {
+            uint256 probability = rand % 100;
+            uint256 index = 9;
+            if (probability < rightWallProbabilities[i]) {
+                // cannot go on the bottom row of the wall
+                uint256 position = (rand % 6) + 3;
+                (newGrid, index) = pickPosition(newGrid, rightWallTraits[i], position);
+            }
+            rand = rand / 100;
+            viewTraitPositons[i] = index;
+        }
+        return viewTraitPositons;
     }
 
     // alternative to generateLeftWallPositions
-    function getLWPositions(uint256 _tokenId) public view returns (uint[] memory) {
-        uint uintVrf = _VRF.getNumberFromId(_tokenId);
+    function getLWPositions(uint256 _tokenId) public view returns (uint256[] memory) {
+        uint256 uintVrf = _VRF.getNumberFromId(_tokenId);
         bytes memory vrf = uintToBytes(uintVrf);
         vrf = slice(vrf, 1 + rightWallTraits.length * 2, leftWallTraits.length * 2);
         uint256 rand = uint256(keccak256(vrf));
         // start with an empty grid
         string[9] memory newGrid = noRow1Grid;
-        uint[] memory viewTraitPositons = new uint[](leftWallTraits.length);
+        uint256[] memory viewTraitPositons = new uint256[](leftWallTraits.length);
         for (uint256 i = 0; i < leftWallTraits.length; i++) {
             uint256 probability = rand % 100;
             uint256 index = 9;
@@ -119,14 +119,14 @@ contract Metadata is Ownable {
     }
 
     // alternative to generateFloorPositions
-    function getFPositions(uint256 _tokenId) public view returns (uint[] memory) {
-        uint uintVrf = _VRF.getNumberFromId(_tokenId);
+    function getFPositions(uint256 _tokenId) public view returns (uint256[] memory) {
+        uint256 uintVrf = _VRF.getNumberFromId(_tokenId);
         bytes memory vrf = uintToBytes(uintVrf);
         vrf = slice(vrf, 1 + rightWallTraits.length * 2 + leftWallTraits.length * 2, floorTraits.length * 2);
         uint256 rand = uint256(keccak256(vrf));
         // start with an empty grid
         string[9] memory newGrid = emptyGrid;
-        uint[] memory viewTraitPositons = new uint[](floorTraits.length);
+        uint256[] memory viewTraitPositons = new uint256[](floorTraits.length);
         for (uint256 i = 0; i < floorTraits.length; i++) {
             uint256 probability = rand % 100;
             uint256 index = 9;
@@ -141,9 +141,13 @@ contract Metadata is Ownable {
     }
 
     // slice array of bytes
-    function slice(bytes memory data, uint start, uint len) internal pure returns (bytes memory) {
+    function slice(
+        bytes memory data,
+        uint256 start,
+        uint256 len
+    ) internal pure returns (bytes memory) {
         bytes memory b = new bytes(len);
-        for (uint i = 0; i < len; i++) {
+        for (uint256 i = 0; i < len; i++) {
             b[i] = data[i + start];
         }
         return b;
@@ -161,70 +165,72 @@ contract Metadata is Ownable {
 
     function uintToBytes(uint256 x) public pure returns (bytes memory b) {
         b = new bytes(32);
-        assembly { mstore(add(b, 32), x) } //  first 32 bytes = length of the bytes value
+        assembly {
+            mstore(add(b, 32), x)
+        } //  first 32 bytes = length of the bytes value
     }
 
-    function combineUintArrays(uint[] memory a, uint[] memory b) public pure returns (uint[] memory) {
-      uint[] memory c = new uint[](a.length + b.length);
-      for (uint i = 0; i < a.length; i++) {
-              c[i] = a[i];
-      }
-      for (uint i = 0; i < b.length; i++) {
-              c[i + a.length] = b[i];
-      }
-      return c;
+    function combineUintArrays(uint256[] memory a, uint256[] memory b) public pure returns (uint256[] memory) {
+        uint256[] memory c = new uint256[](a.length + b.length);
+        for (uint256 i = 0; i < a.length; i++) {
+            c[i] = a[i];
+        }
+        for (uint256 i = 0; i < b.length; i++) {
+            c[i + a.length] = b[i];
+        }
+        return c;
     }
 
     // alternative selctShell
-    function getShell() public pure returns (uint[] memory shellNumber) {
+    function getShell() public pure returns (uint256[] memory shellNumber) {
         uint256 shell = 8;
-        uint[] memory shellArray = new uint[](1);
+        uint256[] memory shellArray = new uint256[](1);
         shellArray[0] = shell;
         return shellArray;
     }
 
     // get all indexes for a given tokenId by combining arrays
-    function getIndexes(uint256 _tokenId) public view returns (uint[] memory indexes) {
-        uint[] memory allIndexes = combineUintArrays(getShell(), combineUintArrays(getRWPositions(_tokenId), combineUintArrays(getLWPositions(_tokenId), getFPositions(_tokenId))));
+    function getIndexes(uint256 _tokenId) public view returns (uint256[] memory indexes) {
+        uint256[] memory allIndexes = combineUintArrays(
+            getShell(),
+            combineUintArrays(
+                getRWPositions(_tokenId),
+                combineUintArrays(getLWPositions(_tokenId), getFPositions(_tokenId))
+            )
+        );
         return allIndexes;
     }
 
     //["f/altar", "f/props", "l/frame", "r/frame", "r/clock", "s/shell"]
     // Function build metadata for a given token
     function buildMetadata(uint256 _tokenId) public view returns (string memory) {
-			string memory jsonInitial = string.concat(
-					'{"name": "Clifford # ',
-					Strings.toString(_tokenId),
-					'", "description": "Clifford nft description", "attributes": [{"trait_type": "Shell", "value":"',
-					Strings.toString(getShell()[0]),
-					'"}, {"trait_type": "Right Frame", "value":"',
-					Strings.toString(getRWPositions(_tokenId)[0]),
-					'"}, {"trait_type": "Right Clock", "value":"',
-					Strings.toString(getRWPositions(_tokenId)[1]),
-					'"}, {"trait_type": "Left Frame", "value":"',
-					Strings.toString(getLWPositions(_tokenId)[0]),
-					'"}, {"trait_type": "Altar", "value":"',
-					Strings.toString(getFPositions(_tokenId)[0]),
-					'"}, {"trait_type": "Props", "value":"',
-					Strings.toString(getFPositions(_tokenId)[1]),
-					'"}],',
-          '"image": "',
-          _imageURI,
-          Strings.toString(_tokenId),
-          '.png", "animation_url": "data:text/html;base64,'
-			);
+        string memory jsonInitial = string.concat(
+            '{"name": "Clifford # ',
+            Strings.toString(_tokenId),
+            '", "description": "Clifford nft description", "attributes": [{"trait_type": "Shell", "value":"',
+            Strings.toString(getShell()[0]),
+            '"}, {"trait_type": "Right Frame", "value":"',
+            Strings.toString(getRWPositions(_tokenId)[0]),
+            '"}, {"trait_type": "Right Clock", "value":"',
+            Strings.toString(getRWPositions(_tokenId)[1]),
+            '"}, {"trait_type": "Left Frame", "value":"',
+            Strings.toString(getLWPositions(_tokenId)[0]),
+            '"}, {"trait_type": "Altar", "value":"',
+            Strings.toString(getFPositions(_tokenId)[0]),
+            '"}, {"trait_type": "Props", "value":"',
+            Strings.toString(getFPositions(_tokenId)[1]),
+            '"}],',
+            '"image": "',
+            _imageURI,
+            Strings.toString(_tokenId),
+            '.png", "animation_url": "data:text/html;base64,'
+        );
 
-			string memory jsonFinal = Base64.encode(
-					bytes(
-							string.concat(
-									jsonInitial,
-									_compose.composeHTML(getIndexes(_tokenId)),
-									'"}'
-							)
-					)
-			);
+        string memory jsonFinal = Base64.encode(
+            bytes(string.concat(jsonInitial, _compose.composeHTML(getIndexes(_tokenId)), '"}'))
+        );
 
-			string memory output = string.concat("data:application/json;base64,", jsonFinal);
-			return output;
-		}
+        string memory output = string.concat("data:application/json;base64,", jsonFinal);
+        return output;
+    }
 }
--- contracts/VRFv2Consumer.sol
+++ contracts/VRFv2Consumer.sol
@@ -13,106 +13,106 @@ import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
  */
 
 contract VRFv2Consumer is VRFConsumerBaseV2 {
-  VRFCoordinatorV2Interface COORDINATOR;
-	LinkTokenInterface LINKTOKEN;
+    VRFCoordinatorV2Interface COORDINATOR;
+    LinkTokenInterface LINKTOKEN;
 
-  // emit tokenId when random number is generated
-  event RandomNumberGenerated(uint256 tokenId);
+    // emit tokenId when random number is generated
+    event RandomNumberGenerated(uint256 tokenId);
 
-  // Goerli coordinator. For other networks,
-  // see https://docs.chain.link/docs/vrf-contracts/#configurations
-  address vrfCoordinator = 0x2Ca8E0C643bDe4C2E08ab1fA0da3401AdAD7734D;
+    // Goerli coordinator. For other networks,
+    // see https://docs.chain.link/docs/vrf-contracts/#configurations
+    address vrfCoordinator = 0x2Ca8E0C643bDe4C2E08ab1fA0da3401AdAD7734D;
 
-	// Goerli LINK token contract. For other networks, see
-  // https://docs.chain.link/docs/vrf-contracts/#configurations
-  address link_token_contract = 0x326C977E6efc84E512bB9C30f76E30c160eD06FB;
+    // Goerli LINK token contract. For other networks, see
+    // https://docs.chain.link/docs/vrf-contracts/#configurations
+    address link_token_contract = 0x326C977E6efc84E512bB9C30f76E30c160eD06FB;
 
-  // The gas lane to use, which specifies the maximum gas price to bump to.
-  // For a list of available gas lanes on each network,
-  // see https://docs.chain.link/docs/vrf-contracts/#configurations
-  bytes32 keyHash = 0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15;
+    // The gas lane to use, which specifies the maximum gas price to bump to.
+    // For a list of available gas lanes on each network,
+    // see https://docs.chain.link/docs/vrf-contracts/#configurations
+    bytes32 keyHash = 0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15;
 
-  // Depends on the number of requested values that you want sent to the
-  // fulfillRandomWords() function. Storing each word costs about 20,000 gas,
-  // so 100,000 is a safe default for this example contract. Test and adjust
-  // this limit based on the network that you select, the size of the request,
-  // and the processing of the callback request in the fulfillRandomWords()
-  // function.
-  uint32 callbackGasLimit = 100000;
+    // Depends on the number of requested values that you want sent to the
+    // fulfillRandomWords() function. Storing each word costs about 20,000 gas,
+    // so 100,000 is a safe default for this example contract. Test and adjust
+    // this limit based on the network that you select, the size of the request,
+    // and the processing of the callback request in the fulfillRandomWords()
+    // function.
+    uint32 callbackGasLimit = 100000;
 
-  // The default is 3, but you can set this higher.
-  uint16 requestConfirmations = 3;
+    // The default is 3, but you can set this higher.
+    uint16 requestConfirmations = 3;
 
-  // For this example, retrieve 2 random values in one request.
-  // Cannot exceed VRFCoordinatorV2.MAX_NUM_WORDS.
-  uint32 numWords =  1;
+    // For this example, retrieve 2 random values in one request.
+    // Cannot exceed VRFCoordinatorV2.MAX_NUM_WORDS.
+    uint32 numWords = 1;
 
-	// Contract that calls the mint function
-  address minter;
+    // Contract that calls the mint function
+    address minter;
 
-	// Contract that deploys the contracts
-	// remaining LINK will be withdrawn to this address after minting
-  address s_owner;
+    // Contract that deploys the contracts
+    // remaining LINK will be withdrawn to this address after minting
+    address s_owner;
 
-	// Subscription Id set during deployment
-	uint64 public s_subscriptionId = 2255;
+    // Subscription Id set during deployment
+    uint64 public s_subscriptionId = 2255;
 
-	mapping(uint256 => uint256) public requestIdToTokenId;
-	mapping(uint256 => uint256) public tokenIdToRandomWord;
+    mapping(uint256 => uint256) public requestIdToTokenId;
+    mapping(uint256 => uint256) public tokenIdToRandomWord;
 
-  // get the random number from the token id
-  function getNumberFromId(uint256 tokenId) public view returns (uint256) {
-    return tokenIdToRandomWord[tokenId];
-  }
+    // get the random number from the token id
+    function getNumberFromId(uint256 tokenId) public view returns (uint256) {
+        return tokenIdToRandomWord[tokenId];
+    }
 
-  constructor(address mintContract) VRFConsumerBaseV2(vrfCoordinator) {
-    COORDINATOR = VRFCoordinatorV2Interface(vrfCoordinator);
-		LINKTOKEN = LinkTokenInterface(link_token_contract);
-    minter = mintContract;
-		s_owner = msg.sender;
-  }
+    constructor(address mintContract) VRFConsumerBaseV2(vrfCoordinator) {
+        COORDINATOR = VRFCoordinatorV2Interface(vrfCoordinator);
+        LINKTOKEN = LinkTokenInterface(link_token_contract);
+        minter = mintContract;
+        s_owner = msg.sender;
+    }
 
-  // Assumes the subscription is funded sufficiently.
-  function requestRandomWords(uint _tokenId) external {
-    // Will revert if subscription is not set and funded.
-		uint s_requestId = COORDINATOR.requestRandomWords(
-      keyHash,
-      s_subscriptionId,
-      requestConfirmations,
-      callbackGasLimit,
-      numWords
-    );
-		requestIdToTokenId[s_requestId] = _tokenId;
-  }
+    // Assumes the subscription is funded sufficiently.
+    function requestRandomWords(uint256 _tokenId) external {
+        // Will revert if subscription is not set and funded.
+        uint256 s_requestId = COORDINATOR.requestRandomWords(
+            keyHash,
+            s_subscriptionId,
+            requestConfirmations,
+            callbackGasLimit,
+            numWords
+        );
+        requestIdToTokenId[s_requestId] = _tokenId;
+    }
 
-  function fulfillRandomWords(
-    uint256 requestId, /* requestId */
-    uint256[] memory randomWords
-  ) internal override {
-		uint tokenId = requestIdToTokenId[requestId];
-		tokenIdToRandomWord[tokenId] = randomWords[0];
-    emit RandomNumberGenerated(tokenId);
-  }
+    function fulfillRandomWords(
+        uint256 requestId, /* requestId */
+        uint256[] memory randomWords
+    ) internal override {
+        uint256 tokenId = requestIdToTokenId[requestId];
+        tokenIdToRandomWord[tokenId] = randomWords[0];
+        emit RandomNumberGenerated(tokenId);
+    }
 
-  function cancelSubscription(address receivingWallet) external onlyOwner {
-    // Cancel the subscription and send the remaining LINK to a wallet address.
-    COORDINATOR.cancelSubscription(s_subscriptionId, receivingWallet);
-    s_subscriptionId = 0;
-  }
+    function cancelSubscription(address receivingWallet) external onlyOwner {
+        // Cancel the subscription and send the remaining LINK to a wallet address.
+        COORDINATOR.cancelSubscription(s_subscriptionId, receivingWallet);
+        s_subscriptionId = 0;
+    }
 
-  // Transfer this contract's funds to an address.
-  // 1000000000000000000 = 1 LINK
-  function withdraw(uint256 amount, address to) external onlyOwner {
-    LINKTOKEN.transfer(to, amount);
-  }
+    // Transfer this contract's funds to an address.
+    // 1000000000000000000 = 1 LINK
+    function withdraw(uint256 amount, address to) external onlyOwner {
+        LINKTOKEN.transfer(to, amount);
+    }
 
-  modifier onlyMintContract() {
-    require(msg.sender == minter);
-    _;
-  }
+    modifier onlyMintContract() {
+        require(msg.sender == minter);
+        _;
+    }
 
-  modifier onlyOwner() {
-    require(msg.sender == s_owner);
-    _;
-  }
+    modifier onlyOwner() {
+        require(msg.sender == s_owner);
+        _;
+    }
 }
