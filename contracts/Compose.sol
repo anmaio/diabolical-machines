eabc4d35fa9795b4a0001050146d0d1876fecd58
Danny <dannyp@anma.io>
2022-09-30 16:49:45 +0000

    render in correct order

--- contracts/Compose.sol
+++ contracts/Compose.sol
@@ -11,24 +11,24 @@ contract Compose {
     // string[] public objects = [RIGHT_FRAME, RIGHT_CLOCK, LEFT_FRAME, ALTAR, PROPS];
 
     string[18] public floorGridArray = [
-      "280.666386", // 5
-      "421", // 5
-      "421", // 6
-      "359.534", // 6
-      "561.32772", // 7
-      "297.50386", // 7
-      "143.14000000000001", // 8
-      "359.534", // 8
-      "280.666386", // 9
-      "297.50386", // 9
-      "421", // 1
+      "280.666386", // 0
+      "174.014456", // 0
+      "143.14000000000001", // 1
       "235.76000000000002", // 1
       "2", // 2
       "297.50386", // 2
-      "143.14000000000001", // 3
+      "421", // 3
       "235.76000000000002", // 3
       "280.666386", // 4
-      "174.014456" // 4
+      "297.50386", // 4
+      "143.14000000000001", // 5
+      "359.534", // 5
+      "561.32772", // 6
+      "297.50386", // 6
+      "421", // 7
+      "359.534", // 7
+      "280.666386", // 8
+      "421" // 8
     ];
 
     string[18] public leftWallGridArray = [
@@ -78,54 +78,104 @@ contract Compose {
     }
 
     // compose SVG
-    function composeSVG(uint256[] memory positions) public view returns (string memory) {
+    function composeSVG(string[][3] memory objects, uint[] memory indexes) public view returns (string memory) {
         string memory svgStart = _sharedAssets.getSvgStart();
-        string memory data = composeData(positions);
+        string memory data = composeData(objects, indexes);
         string memory svgEnd = _sharedAssets.getSvgEnd();
         // return all svg's concatenated together and base64 encoded
         return Base64.encode(bytes(string.concat(svgStart, data, svgEnd)));
     }
 
-    function composeData(uint256[] memory positions) public view returns (string memory) {
+    function composeData(string[][3] memory objects, uint[] memory indexes) public view returns (string memory) {
       string memory output;
-      for (uint256 i = 0; i < positions.length; i++) {
-        if (positions[i] != 9) {
-          output = string.concat(
-            output,
-            _sharedAssets.getGStart()
-          );
-          // right wall
-          if (i < 2) {
-            output = string.concat(
-              output,
-              rightWallGridArray[2 * positions[i]],
-              ",",
-              rightWallGridArray[2 * positions[i] + 1]
-            );
-          } else if (i == 2) {
-            output = string.concat(
-              output,
-              leftWallGridArray[2 * positions[i]],
-              ",",
-              leftWallGridArray[2 * positions[i] + 1]
-            );
-          } else {
-            output = string.concat(
-              output,
-              floorGridArray[2 * positions[i]],
-              ",",
-              floorGridArray[2 * positions[i] + 1]
-            );
-          }
+      
+      // string[2] memory testRObjects = ["frame", "clock"];
+      // string[1] memory testLObjects = ["frame"];
+      // string[2] memory testFObjects = ["props", "altar"];
 
+      for (uint i = 0; i < indexes.length; i++) {
+        output = string.concat(
+          output,
+          _sharedAssets.getGStart()
+        );
+        // rw
+        if (i < 2) {
           output = string.concat(
             output,
-            _sharedAssets.getGMid(),
-            _sharedAssets.getObjects(i),
-            _sharedAssets.getGEnd()
+            rightWallGridArray[2 * indexes[i]],
+            ",",
+            rightWallGridArray[2 * indexes[i] + 1]
+          );
+        // lw
+        } else if (i < 3) {
+          output = string.concat(
+            output,
+            leftWallGridArray[2 * indexes[i]],
+            ",",
+            leftWallGridArray[2 * indexes[i] + 1]
+          );
+        // floor
+        } else {
+          output = string.concat(
+            output,
+            floorGridArray[2 * indexes[i]],
+            ",",
+            floorGridArray[2 * indexes[i] + 1]
           );
         }
+
+        output = string.concat(
+          output,
+          _sharedAssets.getGMid(),
+          _sharedAssets.getObjects(i),
+          _sharedAssets.getGEnd()
+        );
+        
       }
       return output;
     }
+
+    // function composeData(string[] memory rwGrid, string[] memory lwGrid, string[] memory fGrid) public view returns (string memory) {
+    //   string memory output;
+    //   for (uint256 i = 0; i < positions.length; i++) {
+    //     if (positions[i] != 9) {
+    //       output = string.concat(
+    //         output,
+    //         _sharedAssets.getGStart()
+    //       );
+    //       // right wall
+    //       if (i < 2) {
+    //         output = string.concat(
+    //           output,
+    //           rightWallGridArray[2 * positions[i]],
+    //           ",",
+    //           rightWallGridArray[2 * positions[i] + 1]
+    //         );
+    //       } else if (i == 2) {
+    //         output = string.concat(
+    //           output,
+    //           leftWallGridArray[2 * positions[i]],
+    //           ",",
+    //           leftWallGridArray[2 * positions[i] + 1]
+    //         );
+    //       } else if (i == 3) {
+    //         // floor object 1 has higher index(needs to be placed first)
+    //         output = string.concat(
+    //           output,
+    //           floorGridArray[2 * positions[i]],
+    //           ",",
+    //           floorGridArray[2 * positions[i] + 1]
+    //         );
+    //       }
+
+    //       output = string.concat(
+    //         output,
+    //         _sharedAssets.getGMid(),
+    //         _sharedAssets.getObjects(i),
+    //         _sharedAssets.getGEnd()
+    //       );
+    //     }
+    //   }
+    //   return output;
+    // }
 }
