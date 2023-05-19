c65d8b1fcb52aebde888a22c3cac5134494e5dc6
Zac Williams <williamszac20@gmail.com>
2023-05-19 13:16:08 +0000

    post audit changes

---
+++ src/libraryDeployments/Deployment1.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.16;
+
+contract Deployment1 {
+
+  function getPart() external pure returns (string memory) {
+    return string.concat(
+
+    );
+  }
+}
